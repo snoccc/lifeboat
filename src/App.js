c88b293e0db2a83fecb7d2f2776bc44b858a4d9e
snocc <snoccdev@gmail.com>
2022-09-03 22:37:22 +0200

    clean project & added tailwind

--- src/App.js
+++ src/App.js
@@ -1,23 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
+import './styles/reset.css';
+import './styles/screen.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <main className='bg-blue-300'>
+        hello test
+      </main>
     </div>
   );
 }
