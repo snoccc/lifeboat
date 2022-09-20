7177c81297ce9ef7978385d7e08793c224f6b8b1
snocc <snoccdev@gmail.com>
2022-09-21 01:21:41 +0200

    add file type parsing

--- public/parse.js
+++ public/parse.js
@@ -1,8 +1,25 @@
-const file = "testdir/a.py"
+const fs = require('fs');
+const path = require('path');
+const mime = require('mime-types');
+const yaml = require('js-yaml');
+
+const YAML_CONFIG = yaml.load(fs.readFileSync(path.join(__dirname, 'config.yml'), 'utf8'))
 
 function parse(file) {
+    const scripts = new Set();
+    const extension = path.extname(file);
+    const mimetype = mime.lookup(file);
 
+    if (extension && YAML_CONFIG['extensions'][extension]) {
+        YAML_CONFIG['extensions'][extension].forEach(script => scripts.add(script))
+    }
+
+    if (mimetype) {
+        const type = mimetype.split('/')[0];
+        YAML_CONFIG['mime-types'][type].forEach(script => scripts.add(script))
+    }
+
+    return scripts;
 }
 
-const parsed = parse(file);
-console.log(parsed)
+console.log(parse("testdir/a.mp4"));
